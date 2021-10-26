fe5cc716a5da95a8dfafec37ba5341d3cc9e0e9f
NagatoPeinI1 <shalabhnegi123@gmail.com>
2021-10-26 21:55:57 +0530

    temp_data

--- src/generated/model/account.model.ts
+++ src/generated/model/account.model.ts
@@ -1,6 +1,4 @@
-import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, OneToMany as OneToMany_} from "typeorm"
-import * as marshal from "../marshal"
-import {HistoricalBalance} from "./historicalBalance.model"
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_} from "typeorm"
 
 @Entity_()
 export class Account {
@@ -8,18 +6,9 @@ export class Account {
     Object.assign(this, props)
   }
 
-  /**
-   * Account address
-   */
   @PrimaryColumn_()
   id!: string
 
-  @Column_("text", {nullable: false})
-  wallet!: string
-
-  @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
-  balance!: bigint
-
-  @OneToMany_(() => HistoricalBalance, e => e.account)
-  historicalBalances!: HistoricalBalance[]
+  @Column_("bool", {nullable: false})
+  isFunded!: boolean
 }
