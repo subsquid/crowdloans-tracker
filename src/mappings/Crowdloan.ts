b71aaa6b30ddb99d411018a9e43754b57d2b260e
NagatoPeinI1 <shalabhnegi123@gmail.com>
2021-11-05 06:44:45 +0000

    issue with event

--- src/mappings/Crowdloan.ts
+++ src/mappings/Crowdloan.ts
@@ -1,7 +1,7 @@
 import { EventContext, StoreContext } from "@subsquid/hydra-common";
 import { IgnoreParachainIds } from "../constants";
-import { Auction, Chronicle, Contribution, Crowdloan, ParachainLeases } from "../generated/model";
-import { Slots, Crowdloan as TypeCrowdloan } from "../types";
+import { Auction, Chronicle, Contribution,  ParachainLeases, Crowdloan as modelCrowdloan } from "../generated/model";
+import { Slots, Crowdloan as typeCrowdloan  } from "../types";
 import { apiService } from "./helpers/api";
 import { ensureFund, ensureParachain, getOrCreate, getOrUpdate, isFundAddress } from "./helpers/common";
 import { CrowdloanStatus } from "./helpers/types";
@@ -9,34 +9,43 @@ import { parseNumber } from "./helpers/utils";
 
 
 
-// export async function handleCrowdloanContributed({
-//   store,
-//   event,
-//   block,
-//   extrinsic
-// }: EventContext & StoreContext): Promise<void> {
-//   const data = new TypeCrowdloan.ContributedEvent(event).params
-//   const [contributorId, fundIdx, amount] = new TypeCrowdloan.ContributedEvent(event).params
-//   const amtValue = amount.toNumber()
+export async function handleCrowdloanContributed({
+  store,
+  event,
+  block,
+  extrinsic
+}: EventContext & StoreContext): Promise<void> {
+  const [contributorId, fundIdx, amount] = new typeCrowdloan.ContributedEvent(event).params
+  const amtValue = amount.toNumber()
+  // const contribution = await ensureParachain(fundIdx, store)
 
-//   const contribution = await store.find(Contribution, {
-//     where: {id: `${block.height}-${event.id}`},
-//     take: 1
-//   })
 
-//   let api = await apiService();
-//   const parachain = (await api.query.registrar.paras(fundIdx)).toJSON();
+  const data = await ensureParachain(fundIdx.toNumber(), store);
 
-//   contribution[0].account = contributorId.toHex();
-//   // ToDo: why we are not getting id in parachain & fund variable
-//   contribution[0].fund.id = fundIdx.toString();
-//   contribution[0].parachain.id = parachain.id;
-//   contribution[0].amount = BigInt(amtValue);
-//   contribution[0].createdAt = new Date(block.timestamp);
-//   contribution[0].blockNum = block.height;
 
-//   await store.save(contribution);
-// };
+  const paradata = await ensureFund(fundIdx.toNumber(), store, data);
+
+  // const contribution = await store.find(Contribution, {
+  //   where: {blockNum: block.height},
+  //   // where: {id: `${block.height}-${event.id}`},
+  //   take: 1
+  // })
+  // let api = await apiService();
+  // const parachain = (await api.query.registrar.paras(fundIdx)).toJSON() as any;
+  // if(contribution.length != 0) {
+  //   contribution[0].account = contributorId.toHex();
+  //   // ToDo: why we are not getting id in parachain & fund variable
+  //   contribution[0].fund.id = fundIdx.toString();
+  //   contribution[0].parachain.id = parachain.id;
+  //   contribution[0].amount = BigInt(amtValue);
+  //   contribution[0].createdAt = new Date(block.timestamp);
+  //   contribution[0].blockNum = block.height;
+  //   await store.save(contribution);
+  // } else {
+  //     console.info(`unable to find the contribution for parachain:  ${parachain}`)
+  // }
+
+};
 
 
 export async function handleCrowdloanDissolved({
@@ -44,9 +53,9 @@ export async function handleCrowdloanDissolved({
   event,
   block,
 }: EventContext & StoreContext): Promise<void> {
-  const [fundId] = new TypeCrowdloan.DissolvedEvent(event).params;
+  const [fundId] = new typeCrowdloan.DissolvedEvent(event).params;
 
-  const crowdloan = await store.find(Crowdloan, {where: { id: fundId.toString() }, take: 1});
+  const crowdloan = await store.find(modelCrowdloan, {where: { id: fundId.toString() }, take: 1});
 
   crowdloan[0].status = 'Dissolved';
   crowdloan[0].isFinished = true;
