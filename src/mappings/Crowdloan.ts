cf627a429c2e3abeb5c44c525999f5a0d879e8b2
NagatoPeinI1 <shalabhnegi123@gmail.com>
2021-11-08 13:53:43 +0530

    event differentation logs added

--- src/mappings/Crowdloan.ts
+++ src/mappings/Crowdloan.ts
@@ -58,8 +58,9 @@ export async function handleCrowdloanCreated({
   event,
   block
 }: EventContext & StoreContext): Promise<void> {
-  const blockNum = block.height;
+  console.info(` ------ Crowdloan Createdevent Event Startd.`);
   const [fundId] = new Crowdloan.DissolvedEvent(event).params;
   await ensureParachain(fundId.toNumber(), store);
-  const fund = await ensureFund(fundId.toNumber(), store, { blockNum });
+  const fund = await ensureFund(fundId.toNumber(), store, { blockNum: block.height });
+  console.info(` ------ Crowdloan Createdevent Event Completed.`);
 };
