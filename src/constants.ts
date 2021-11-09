f6b35776ec68fd86eace2c75ca1588cb56f91750
sachushaji <sachushajiabraham@gmail.com>
2021-11-09 16:41:56 +0530

    bid handlers

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const IgnoreParachainIds = [100, 110, 120, 1];
+export const ChronicleKey = 'ChronicleKey';
 
 export enum CrowdloanStatus {
   RETIRING = "Retiring",
--- src/mappings/Auction.ts
+++ src/mappings/Auction.ts
@@ -1,9 +1,9 @@
-import { EventContext, StoreContext } from "@subsquid/hydra-common";
-import { Auction, AuctionParachain, Bid, Chronicle, Parachain } from "../generated/model";
+import { DatabaseManager, EventContext, StoreContext, SubstrateBlock } from "@subsquid/hydra-common";
+import { Auction, AuctionParachain, Bid, Chronicle, Parachain, ParachainLeases } from "../generated/model";
 import { Auctions } from "../types";
 import { apiService } from "./helpers/api";
+import { ChronicleKey } from '../constants';
 import { ensureFund, ensureParachain, getByAuctionParachain, getByAuctions, getLatestCrowdloanId, getOrCreate, getOrUpdate, isFundAddress } from "./helpers/common";
-import { getParachainId } from "./helpers/utils";
 
 export async function handlerEmpty () {};
 
@@ -17,9 +17,9 @@ export async function handleAuctionStarted({
   const [auctionId, slotStart, auctionEnds] = new Auctions.AuctionStartedEvent(event).params;
 
   const api = await apiService();
-  const endingPeriod = api.consts.auctions.endingPeriod.toJSON() as number;
-  const leasePeriod = api.consts.slots.leasePeriod.toJSON() as number;
-  const periods = api.consts.auctions.leasePeriodsPerSlot.toJSON() as number;
+  const endingPeriod =await  api.consts.auctions.endingPeriod.toJSON() as number;
+  const leasePeriod = await api.consts.slots.leasePeriod.toJSON() as number;
+  const periods = await api.consts.auctions.leasePeriodsPerSlot.toJSON() as number;
 
   const auction = await getOrCreate(store, Auction, auctionId.toString());
 
@@ -52,9 +52,9 @@ export async function handleAuctionClosed({
   const auction = await getOrCreate(store, Auction, auctionId.toString());
 
   let api = await apiService();
-  const endingPeriod = api.consts.auctions.endingPeriod.toJSON() as number;
-  const periods = api.consts.auctions.leasePeriodsPerSlot.toJSON() as number;
-  const leasePeriod = api.consts.slots.leasePeriod.toJSON() as number;
+  const endingPeriod = await api.consts.auctions.endingPeriod.toJSON() as number;
+  const periods = await api.consts.auctions.leasePeriodsPerSlot.toJSON() as number;
+  const leasePeriod = await api.consts.slots.leasePeriod.toJSON() as number;
 
   auction.blockNum = block.height;
   auction.status = "Closed";
@@ -93,87 +93,228 @@ export async function handleAuctionWinningOffset ({
   console.info(` ------ [Auctions] [WinningOffset] Event Completed.`);
 };
 
-export async function handleBidAccepted({
+
+const markLosingBids = async (auctionId: number, slotStart: number, slotEnd: number, winningBidId: string, store: DatabaseManager) => {
+  const winningBids = (await store.find(Bid, {
+    where: {winningAuction: auctionId}
+  }))
+  const losingBids = winningBids?.filter(
+    ({ firstSlot, lastSlot, id }) => id !== winningBidId && slotStart == firstSlot && slotEnd == lastSlot
+  ) ||  []
+  for (const bid of losingBids) {
+    bid.winningAuction = null;
+    await store.save(bid)
+    console.info(`Mark Bid as losing bid ${bid.id}`);
+  }
+};
+
+const markParachainLeases = async (
+  auctionId: number,
+  paraId: number,
+  leaseStart: number,
+  leaseEnd: number,
+  bidAmount: number,
+  store: DatabaseManager
+) => {
+  const leaseRange = `${auctionId}-${leaseStart}-${leaseEnd}`;
+  const { id: parachainId } = await ensureParachain(paraId, store);
+  const winningLeases = await store.find(ParachainLeases,
+    {
+      where: {leaseRange: leaseRange}
+    })
+  const losingLeases = winningLeases?.filter((lease) => lease.paraId !== paraId) || []
+  for (const lease of losingLeases) {
+    lease.activeForAuction = null;
+    store.save(lease)
+    console.info(`Mark losing parachain leases ${lease.paraId} for ${lease.leaseRange}`);
+  }
+  await getOrUpdate(store, ParachainLeases, `${paraId}-${leaseRange}`, {
+    paraId,
+    leaseRange,
+    parachainId,
+    firstLease: leaseStart,
+    lastLease: leaseEnd,
+    auctionId: auctionId?.toString(),
+    latestBidAmount: bidAmount,
+    activeForAuction: auctionId?.toString(),
+    hasWon: false
+  });
+};
+
+/**
+ *
+ * @param substrateEvent SubstrateEvent
+ * Create Bid record and create auction parachain record if not exists already
+ * Skip winning bid before we have query abilities
+ */
+export const handleBidAccepted = async ({
   store,
   event,
   block,
-}: EventContext & StoreContext): Promise<void> {
-  console.info(` ------ [Auctions] [BidAccepted] Event Started.`);
-  
-  const { timestamp: createdAt, height: blockNum, id: blockId  } = block;
-  const [from, paraId, amount, firstSlot, lastSlot] = new Auctions.BidAcceptedEvent(event).params;
-  const api = await apiService();
+}: EventContext & StoreContext): Promise<void> => {
+const api = await apiService()
+  const blockNum = block.height
+  const [from, paraId, amount, firstSlot, lastSlot] = new Auctions.BidAcceptedEvent(event).params
   const auctionId = (await api.query.auctions.auctionCounter()).toJSON() as number;
+  const isFund = await isFundAddress(from.toString());
+  const parachain = await ensureParachain(paraId.toNumber(), store);
+  const { id: parachainId } = parachain;
 
+  const fundId = await getLatestCrowdloanId(parachainId, store);
+  const bidAmount = amount.toNumber()
+  const bid = {
+    id: `${blockNum}-${from}-${paraId}-${firstSlot}-${lastSlot}`,
+    auctionId: `${auctionId}`,
+    blockNum,
+    winningAuction: auctionId,
+    parachainId,
+    isCrowdloan: isFund,
+    amount: amount.toNumber(),
+    firstSlot,
+    lastSlot,
+    createdAt : block.timestamp,
+    fundId: isFund ? fundId : null,
+    bidder: isFund ? null : from
+  };
 
-  // const auction = await getOrCreate(store, Auction, auctionId.toString());
-  // const parachainId = (await getParachainId(paraId.toNumber())) as any;
-  // const parachain = await ensureParachain(paraId.toNumber(), store);
-  const { id: parachainId } = await ensureParachain(paraId.toNumber(), store);
+  await store.save(bid);
 
-  const isFund = await isFundAddress(from.toHex());
-  const fund = await ensureFund(paraId.toNumber(), store);
-  const fundIdAlpha = await getLatestCrowdloanId(paraId.toString(), store);
+  await markParachainLeases(auctionId, paraId.toNumber(), firstSlot.toNumber(), lastSlot.toNumber(), bidAmount, store);
 
+  await markLosingBids(auctionId, firstSlot.toNumber(), lastSlot.toNumber(), bid.id, store);
 
-  const parachain = await store.find(Parachain, {
-    where: { id: parachainId },
-    take: 1,
+  const auctionParaId = `${paraId}-${firstSlot}-${lastSlot}-${auctionId}`;
+  const auctionPara = await store.find(AuctionParachain, {
+    where: {id: auctionParaId}
+  })
+  if (!auctionPara) {
+    let parachain = await store.find(Parachain, {
+      where:{id: parachainId}
+    })
+    let auction = await store.find(Auction, {
+      where: {id: auctionId.toString()}
+    })
+    let newAuctionPara = new AuctionParachain({
+        id: `${paraId}-${firstSlot}-${lastSlot}-${auctionId}`,
+        parachain: parachain[0],
+        auction: auction[0],
+        firstSlot: firstSlot.toNumber(),
+        lastSlot: lastSlot.toNumber(),
+        createdAt: new Date(block.timestamp),
+        blockNum
+    })
+     await store.save(newAuctionPara);
+  }
+};
+
+export const updateBlockNum = async (block: SubstrateBlock, store : DatabaseManager) => {
+  await getOrUpdate<Chronicle>(store, Chronicle, ChronicleKey, {
+    curBlockNum: block.height
   });
+};
 
-  const parachain2 = await store.find(Parachain, {
-    where: { id: paraId },
-    take: 1,
-  });
+export const updateWinningBlocks = async (block: SubstrateBlock, store: DatabaseManager) => {
+  const { curAuctionId, curBlockNum  } = (await store.find(Chronicle,{
+where: {id:ChronicleKey}
+  }))[0] || {};
+  const { closingStart, closingEnd } = (await store.find(Auction,{
+    where: {id:curAuctionId || ''}
+      }))[0] || {};
+      let currentBlockNumber = curBlockNum || -1;
+  if (curAuctionId && currentBlockNumber >= closingStart && currentBlockNumber < closingEnd) {
+    const winningLeases = await store.find(ParachainLeases, {
+      where: { id: curAuctionId}});
+    for (const lease of winningLeases) {
+      lease.numBlockWon = (lease.numBlockWon || 0) + 1;
+      await store.save(lease)
+    }
+  }
+};
 
-  // const auction = await store.find(Auction, {
-  //   where: { id: auctionId.toString() },
+// export async function handleBidAccepted({
+//   store,
+//   event,
+//   block,
+// }: EventContext & StoreContext): Promise<void> {
+//   console.info(` ------ [Auctions] [BidAccepted] Event Started.`);
+  
+  
+  
+  
+  // const { timestamp: createdAt, height: blockNum, id: blockId  } = block;
+  // const [from, paraId, amount, firstSlot, lastSlot] = new Auctions.BidAcceptedEvent(event).params;
+  // const api = await apiService();
+  // const auctionId = (await api.query.auctions.auctionCounter()).toJSON() as number;
+
+
+  // // const auction = await getOrCreate(store, Auction, auctionId.toString());
+  // // const parachainId = (await getParachainId(paraId.toNumber())) as any;
+  // // const parachain = await ensureParachain(paraId.toNumber(), store);
+  // const { id: parachainId } = await ensureParachain(paraId.toNumber(), store);
+
+  // const isFund = await isFundAddress(from.toHex());
+  // const fund = await ensureFund(paraId.toNumber(), store);
+  // const fundIdAlpha = await getLatestCrowdloanId(paraId.toString(), store);
+
+
+  // const parachain = await store.find(Parachain, {
+  //   where: { id: parachainId },
   //   take: 1,
   // });
 
-  const auctionData = await getByAuctions(store, auctionId.toString()) as Auction[];
-  if(auctionData.length != 0) {
+  // const parachain2 = await store.find(Parachain, {
+  //   where: { id: paraId },
+  //   take: 1,
+  // });
 
-    let auction = auctionData[0]
-  
-    const bid = new Bid({
-      id: `${blockNum}-${from}-${parachainId}-${firstSlot}-${lastSlot}`,
-      auction,
-      blockNum,
-      winningAuction: auctionId,
-      parachain: parachain[0],
-      isCrowdloan: isFund,
-      amount: BigInt(amount.toString()),
-      firstSlot: firstSlot.toNumber(),
-      lastSlot: lastSlot.toNumber(),
-      createdAt: new Date(createdAt),
-      fund,
-      bidder: isFund ? null : from.toHex(),
-    });
-  
-    console.log(" bid ::: ",bid)
-    /**
-     * ToDo: Getting error :-
-              name: QueryFailedError, message: insert or update on table "bid" violates foreign key constraint "FK_9e594e5a61c0f3cb25679f6ba8d", 
-              stack: QueryFailedError: insert or update on table "bid" violates foreign key constraint "FK_9e594e5a61c0f3cb25679f6ba8d"
-     *  */ 
-    await store.save(bid);
-    // const auctionParaId = `${paraId}-${firstSlot}-${lastSlot}-${auctionId}`;
-    // const auctionPara = await store.get(AuctionParachain,{
-    //   where: { auctionParaId }
-    // });
-    // if (!auctionPara) {
-    //   await store.save(new AuctionParachain({
-    //     id: auctionParaId,
-    //     firstSlot: firstSlot.toNumber(),
-    //     lastSlot: lastSlot.toNumber(),
-    //     createdAt: new Date(block.timestamp),
-    //     blockNum: block.height
-    //   }))
-    // }
-  } else {
-    console.log(` ------ [Auctions] [BidAccepted] Event No auction found.`);
-  }
+  // // const auction = await store.find(Auction, {
+  // //   where: { id: auctionId.toString() },
+  // //   take: 1,
+  // // });
 
-  console.info(` ------ [Auctions] [BidAccepted] Event Completed.`);
-}
+  // const auctionData = await getByAuctions(store, auctionId.toString()) as Auction[];
+  // if(auctionData.length != 0) {
+
+  //   let auction = auctionData[0]
+  
+  //   const bid = new Bid({
+  //     id: `${blockNum}-${from}-${parachainId}-${firstSlot}-${lastSlot}`,
+  //     auction,
+  //     blockNum,
+  //     winningAuction: auctionId,
+  //     parachain: parachain[0],
+  //     isCrowdloan: isFund,
+  //     amount: BigInt(amount.toString()),
+  //     firstSlot: firstSlot.toNumber(),
+  //     lastSlot: lastSlot.toNumber(),
+  //     createdAt: new Date(createdAt),
+  //     fund,
+  //     bidder: isFund ? null : from.toHex(),
+  //   });
+  
+  //   console.log(" bid ::: ",bid)
+  //   /**
+  //    * ToDo: Getting error :-
+  //             name: QueryFailedError, message: insert or update on table "bid" violates foreign key constraint "FK_9e594e5a61c0f3cb25679f6ba8d", 
+  //             stack: QueryFailedError: insert or update on table "bid" violates foreign key constraint "FK_9e594e5a61c0f3cb25679f6ba8d"
+  //    *  */ 
+  //   await store.save(bid);
+  //   // const auctionParaId = `${paraId}-${firstSlot}-${lastSlot}-${auctionId}`;
+  //   // const auctionPara = await store.get(AuctionParachain,{
+  //   //   where: { auctionParaId }
+  //   // });
+  //   // if (!auctionPara) {
+  //   //   await store.save(new AuctionParachain({
+  //   //     id: auctionParaId,
+  //   //     firstSlot: firstSlot.toNumber(),
+  //   //     lastSlot: lastSlot.toNumber(),
+  //   //     createdAt: new Date(block.timestamp),
+  //   //     blockNum: block.height
+  //   //   }))
+  //   // }
+  // } else {
+  //   console.log(` ------ [Auctions] [BidAccepted] Event No auction found.`);
+  // }
+
+  // console.info(` ------ [Auctions] [BidAccepted] Event Completed.`);
+// }
