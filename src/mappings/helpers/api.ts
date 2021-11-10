3ffa0922a80bd7c5c365467d522351f013f85e75
NagatoPeinI1 <shalabhnegi123@gmail.com>
2021-11-10 17:16:40 +0530

    Subsquid auction indexer for polkadot

--- src/mappings/helpers/api.ts
+++ src/mappings/helpers/api.ts
@@ -2,6 +2,6 @@ import { ApiPromise, WsProvider } from "@polkadot/api"
 let api: ApiPromise | undefined
 export const apiService =  async () => {
     if (api) return api;
-    api = await ApiPromise.create({ provider: new WsProvider('wss://kusama-rpc.polkadot.io/') })
+    api = await ApiPromise.create({ provider: new WsProvider('wss://rpc.polkadot.io/') })
     return api
 }
